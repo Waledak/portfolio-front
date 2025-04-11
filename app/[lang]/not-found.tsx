7cc4076af7bbfe8c2f9320b858138d9bb8317232
Tanguy <tanguy.cirillo@gmail.com>
2025-04-11 18:53:30 +0200

    fix: params error and icon error

--- app/[lang]/not-found.tsx
+++ app/[lang]/not-found.tsx
@@ -1,6 +1,12 @@
+"use client"
+
 import Link from 'next/link'
+import {usePathname} from "next/navigation";
 
 export default function NotFound() {
+    const pathname = usePathname()
+    const lang = pathname.split('/')[1] || 'fr'
+
     return (
         <main className="min-h-screen flex flex-col items-center justify-center text-center p-6">
             <h1 className="text-4xl font-bold">404 – Page not found</h1>
@@ -8,7 +14,7 @@ export default function NotFound() {
                 The page you’re looking for doesn’t exist.
             </p>
             <Link
-                href="/fr"
+                href={`/${lang}`}
                 className="mt-6 px-5 py-2 bg-primary text-white rounded-full hover:bg-primary/80 transition"
             >
                 Go back home
