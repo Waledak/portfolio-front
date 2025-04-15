8ddecc0aa81acb62db3dc5b9b1f23d0d2a2e411f
Tanguy <tanguy.cirillo@gmail.com>
2025-04-16 01:43:10 +0200

    fix: env name

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,6 +1,6 @@
 import {GalleryResponse, HomePage} from "@/types/strapi.type";
 
-const STRAPI_URL = process.env.STRAPI_URL || 'http://localhost:1337'
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337'
 
 export async function getHomeData(locale = 'en'): Promise<HomePage> {
     try {
