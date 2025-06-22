3bdc4c368d3fb04a9f760291d0383a4f67deb0cb
Tanguy <tanguy.cirillo@gmail.com>
2025-06-22 11:31:12 +0200

    fix: text color on timeline content

--- components/timeline/TimelineElementContent.tsx
+++ components/timeline/TimelineElementContent.tsx
@@ -13,12 +13,12 @@ export interface TimelineElementContentProps {
 const TimelineElementContent = ({title, type, description, subtitle, technologies, location}: TimelineElementContentProps) => (
     <div>
         <div className="flex justify-between gap-2 flex-wrap">
-            <h2 className="text-2xl">{title}</h2>
+            <h2 className="text-2xl text-black">{title}</h2>
             <div className="badge badge-outline badge-secondary">{type}</div>
         </div>
 
-        <h3 className="text-lg">{subtitle}</h3>
-        <p className="text-justify my-3">{description}</p>
+        <h3 className="text-lg text-black">{subtitle}</h3>
+        <p className="text-justify my-3 text-black">{description}</p>
         {technologies &&
             <div className="flex my-2 flex-wrap gap-2">
                 {
